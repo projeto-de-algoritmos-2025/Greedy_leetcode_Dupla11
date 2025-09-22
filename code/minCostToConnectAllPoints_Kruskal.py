c32db87ef4dd6d63ea32886d7ee5bcd71b920836
Caio Santos <caiobs10@gmail.com>
2025-09-22 19:08:14 -0300

    Problema nº 1584 resolvido com Kruskal

---
+++ code/minCostToConnectAllPoints_Kruskal.py
@@ -0,0 +1,46 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0]*n
+        self.n = n
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    def union(self, a, b):
+        ra, rb = self.find(a), self.find(b)
+        if ra == rb:
+            return False
+        if self.rank[ra] < self.rank[rb]:
+            self.parent[ra] = rb
+        elif self.rank[ra] > self.rank[rb]:
+            self.parent[rb] = ra
+        else:
+            self.parent[rb] = ra
+            self.rank[ra] += 1
+        self.n -=1
+        return True
+    def isConnected(self):
+      return self.n == 1
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n= len(points)
+        edges = []
+        for i in range(n):
+            xi, yi = points[i]
+            for j in range(i + 1, n):
+                xj, yj = points[j]
+                dist = abs(xi - xj) + abs(yi - yj)
+                edges.append([dist, i, j])
+        
+        edges.sort(key=lambda x: x[0])
+        uf = UnionFind(n)
+        res = 0
+        for cost, a, b in edges:
+            if uf.union(a, b):
+                res += cost
+                if uf.isConnected():
+                    break           
+        return res
+        
