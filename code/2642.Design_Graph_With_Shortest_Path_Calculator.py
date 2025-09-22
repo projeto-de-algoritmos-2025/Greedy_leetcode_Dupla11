b71bea99b69c3465492b187a9dd3c91003f9cc09
Gabriel_Castelo <castelogabriel@outlook.com>
2025-09-22 16:22:59 -0300

    adiciona construção do grafo

---
+++ code/2642.Design_Graph_With_Shortest_Path_Calculator.py
@@ -0,0 +1,25 @@
+import heapq
+from typing import List
+
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]):
+
+        self.graph = [[] for _ in range(n)]
+        for e in edges:
+            self.graph[e[0]].append([e[1], e[2]])
+        pass
+
+    def addEdge(self, edge: List[int]) -> None:
+        self.graph[edge[0]].append([edge[1], edge[2]])
+        pass
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+
+        pass
+
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
