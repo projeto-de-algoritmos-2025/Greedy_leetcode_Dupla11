f64c240ee91cfae51cdec3928ebcc3e4752387b8
Gabriel_Castelo <castelogabriel@outlook.com>
2025-09-22 16:38:14 -0300

    adiciona heap no algoritimo

--- code/2642.Design_Graph_With_Shortest_Path_Calculator.py
+++ code/2642.Design_Graph_With_Shortest_Path_Calculator.py
@@ -1,10 +1,12 @@
 import heapq
 from typing import List
 
+
 class Graph:
 
     def __init__(self, n: int, edges: List[List[int]]):
 
+        self.size = n
         self.graph = [[] for _ in range(n)]
         for e in edges:
             self.graph[e[0]].append([e[1], e[2]])
@@ -15,7 +17,15 @@ class Graph:
         pass
 
     def shortestPath(self, node1: int, node2: int) -> int:
+        dist = [float('inf') for _ in range(self.size)]
+        pq = []
+        heapq.heappush(pq, node1)
+        dist[node1] = 0
 
+        while pq:
+            u = heapq.heappop(pq)
+            for v in self.graph[u]:
+                heapq.heappush(pq,v)
         pass
 
 
