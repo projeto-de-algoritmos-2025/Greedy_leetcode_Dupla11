faac5e45708d01972ac13af2ded9786e9bfd61e6
GabrielCastelo-31 <gabflubr@outlook.com>
2025-09-22 22:26:43 -0300

    solução da questão

--- code/2642.Design_Graph_With_Shortest_Path_Calculator.py
+++ code/2642.Design_Graph_With_Shortest_Path_Calculator.py
@@ -5,31 +5,44 @@ from typing import List
 class Graph:
 
     def __init__(self, n: int, edges: List[List[int]]):
-
+        """Cria o grafo como uma lista de adjacência """
         self.size = n
         self.graph = [[] for _ in range(n)]
         for e in edges:
             self.graph[e[0]].append([e[1], e[2]])
-        pass
 
     def addEdge(self, edge: List[int]) -> None:
+        """ Adiciona uma aresta ao grafo """
         self.graph[edge[0]].append([edge[1], edge[2]])
         pass
 
     def shortestPath(self, node1: int, node2: int) -> int:
+        """ Calcula o caminho mais curto entre dois nós usando Dijkstra com fila de prioridade """
+        # Inicializa a distância de todos os nós como infinito
         dist = [float('inf') for _ in range(self.size)]
+
+        # Cria uma fila de prioridade e adiciona o nó inicial e define sua distância como 0
         pq = []
-        heapq.heappush(pq, node1)
+        heapq.heappush(pq, [0, node1])
         dist[node1] = 0
-
         while pq:
-            u = heapq.heappop(pq)
+            u_weight, u = heapq.heappop(pq)
+
+            # node2 já foi visitado e temos o menor caminho garantido.
+            if u == node2:
+                # pode retonar sem visitar o resto do grafo.
+                return u_weight
+
             for v in self.graph[u]:
-                heapq.heappush(pq,v)
-        pass
+                w, weight = v[0], v[1]
 
+                # Se a nova distância para v for menor que a distancia atual até v
+                if dist[w] > dist[u] + weight:
+                    dist[w] = int(dist[u] + weight)
+                    # Adiciona V na heap
+                    heapq.heappush(pq, [dist[w], w])
 
-# Your Graph object will be instantiated and called as such:
-# obj = Graph(n, edges)
-# obj.addEdge(edge)
-# param_2 = obj.shortestPath(node1,node2)
+        # Se não existe caminho de node1 até node2
+        if dist[node2] == float('inf'):
+            return -1
+        return dist[node2]
